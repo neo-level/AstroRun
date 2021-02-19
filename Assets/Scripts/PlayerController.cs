2f7e54eb76669123bb154c0b996b3c2bf4293e81
frederik desmet <info@neo-level.com>
2021-02-19 13:58:39 +0100

    implemented a jumping mechaninc

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Animator _animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(key: KeyCode.Space))
+        {
+            IsJumping(true);
+        }
+        else
+        {
+            IsJumping(false);
+
+        }
+    }
+
+    private void IsJumping(bool isSpacePressed)
+    {
+        _animator.SetBool("isJumping", isSpacePressed);
     }
 }
